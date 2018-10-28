0393d60f3c82075213c468b4deaffa140dca4ad4
David Lenkner <lenknerd@gmail.com>
2018-10-28 13:15:24 -0400

    Get post request with data in, logging in

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ceiling Game
+
+This just contains personal dev notes for now, nothing intended for anyone but myself
+
+http://192.168.1.8/phpmyadmin
+root
+ubuntu pw
+
+Implementation Notes
+https://docs.google.com/document/d/1Ix0gNQhQaH6uHRNyP6fcoN7G_8oqpHkpdX2ReC5pKsE/edit
+
+Overall Design Initial Notes
+https://docs.google.com/document/d/1_8X6sRpcmu2lFd4JNalXtT1T_WGuGIZfQiRNrX0LnRg/edit
--- tutorial_webapp/app.py
+++ tutorial_webapp/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # Just a test
-from flask import Flask, render_template
 
+import logging
+from flask import Flask, render_template, request
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -16,5 +18,25 @@ def cakes():
     return 'Yummy cakes!'
 
 
+@app.route('/insert_test', methods=['POST'])
+def insert_test():
+    # f = request.form
+    app.logger.debug('hey requested')
+    v = request.values
+    for key in v.keys():
+        app.logger.debug(key)
+        app.logger.debug(v['n'])
+    
+    # app.logger.info('hey here info requested')
+    # for key in f.keys():
+    #    for value in f.getlist(key):
+    #        app.logger.debug(key)
+    #        app.logger.info(key)
+    return "You did a request."
+
+
 if __name__ == '__main__':
+    handler = RotatingFileHandler('ceiling_game_debug.log', maxBytes=100000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
     app.run(debug=True, host='0.0.0.0')
